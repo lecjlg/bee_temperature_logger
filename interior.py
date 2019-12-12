4ae829e2184d36cbc1060c039380da49ba81df0e
James <james.groves@ncas.ac.uk>
2019-12-12 22:47:20 +0000

    removed the a commented out line

--- interior.py
+++ interior.py
@@ -47,7 +47,6 @@ def read_temp():
   if equals_pos != -1:
     temp_string = lines[1][equals_pos+2:]
     temp_c = float(temp_string) / 1000.0
-   #temp_f = temp_c * 9.0 / 5.0 + 32.0
     return temp_c
 
 # Print out the temperature until the program is stopped.
