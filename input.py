288b412a101b1336cd85baa8ad510474e2b3168a
James Groves <james.groves@ncas.ac.uk>
2020-02-29 22:20:09 +0000

    added date and time and file open

--- input.py
+++ input.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
-
+import os
 import time
-
 import automationhat
+import time
+import datetime
+from datetime import datetime
+from datetime import date
+
+filedate = date.today()
+
+outfile='/data/battery/'+ str(filedate) + '-' + 'battery_voltage.csv'
+
+
 
 
 if automationhat.is_automation_hat():
     automationhat.light.power.write(1)
-
-while True:
-    print(automationhat.input.read())
-    print(automationhat.analog.read())
-    time.sleep(0.5)
+with open(outfile,'a') as f:
+    while True:
+    
+        
+        print(automationhat.input.read())
+        print(automationhat.analog.read())
+        time.sleep(0.5)
