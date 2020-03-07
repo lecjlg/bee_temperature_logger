d67948b7ff0f64bb6387188cda3fc5549417721c
James Groves <james.groves@ncas.ac.uk>
2020-03-07 22:00:28 +0000

    added RH to data string

--- sht_log.py
+++ sht_log.py
@@ -16,7 +16,7 @@ sht = Sht(21, 17)
 
 with open(outfile,'a') as f:
     while True :
-        f.write(datetime.utcnow().isoformat() + ',' + str(sht.read_t()) + '\n' ) #write the data to a string and to the output file
+        f.write(datetime.utcnow().isoformat() + ',' + str(sht.read_t()) + ',' + str(sht.read_rh()) + '\n' ) #write the data to a string and to the output file
         print 'Temperature', sht.read_t()
         print 'Relative Humidity', sht.read_rh()
         time.sleep(2.4)
