c6cf82b2d254b39a6ecb1122cdb8ffa437cdb5d9
James Groves <james.groves@ncas.ac.uk>
2020-05-06 09:52:06 +0100

    removed all the print statements

--- bme280.py
+++ bme280.py
@@ -172,14 +172,14 @@ def readBME280All(addr=DEVICE):
 def main():
 
   (chip_id, chip_version) = readBME280ID()
-  print "Chip ID     :", chip_id
-  print "Version     :", chip_version
+  #print "Chip ID     :", chip_id
+  #print "Version     :", chip_version
 
   temperature,pressure,humidity = readBME280All()
 
-  print "Temperature : ", temperature, "C"
-  print "Pressure : ", pressure, "hPa"
-  print "Humidity : ", humidity, "%"
+  #print "Temperature : ", temperature, "C"
+  #print "Pressure : ", pressure, "hPa"
+  #print "Humidity : ", humidity, "%"
 
   temperature = str(temperature)  # float -> str
   pressure = str(pressure)
